432b89933cbaf484117491862d5e7948e4547adc
toricor <toriyabe@mfac.jp>
2020-06-12 10:20:16 +0900

    ch12.3: separete lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
